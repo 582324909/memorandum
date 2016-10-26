b5588d21838b5c96be13dd58b53a42575653a6d9
张伟伟 <zhangweiwei@zhangweiwei.local>
2016-10-26 08:54:21 +0800

    Initial Commit

---
+++ rili/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rili
+//
+//  Created by 张伟伟 on 2016/10/26.
+//  Copyright © 2016年 张伟伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ rili/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rili
+//
+//  Created by 张伟伟 on 2016/10/26.
+//  Copyright © 2016年 张伟伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
